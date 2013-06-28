bbcd5d4da5fef551643852bfb567b1ce45377402
ixiami <ixuhao@yahoo.com>
2013-06-28 16:26:52 +0800

    Update collector.sh

--- collector.sh
+++ collector.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-perl trim.pl | sed 's/Incoming:\[17103HCurrurrent/g' | cut -d[ -f1 | cut -d' ' -f2
+perl trim.pl | sed 's/Incoming:\[17103HCurrurrent/\nCurrent/g' | cut -d[ -f1 | cut -d' ' -f2
