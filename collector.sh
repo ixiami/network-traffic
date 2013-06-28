eb5f0e07a59c0ec75b4d80079bfb37ab0338d65b
ixiami <ixuhao@yahoo.com>
2013-06-28 16:44:57 +0800

    Update collector.sh

--- collector.sh
+++ collector.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-perl trim.pl | sed 's/Incoming:\[17103HCurr/\nCurrent/g' | cut -d[ -f1 | cut -d':' -f2
+perl trim.pl | sed 's/Incoming:\[17103HCurr/\nCurrent/g' | cut -d[ -f1 | cut -d':' -f2 \
+|awk '{if($2 == "Bit/s") {print $1/1000} else if ($2 == "mBit/s"){print $1 * 1000} else {print $1}}'
